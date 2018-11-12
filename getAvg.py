4341e8791ff9373087825f30207227d7aabe328a
rishabghosh <rishabg7269@gmail.com>
2018-11-12 17:19:47 +0530

    now takes two numbers from command line

--- getAvg.py
+++ getAvg.py
@@ -1,7 +1,12 @@
+import sys
+#takes command line arguments in a array of strings
+
 def addNumbers(num1, num2):
     return num1 + num2
 
 def main():
-    print(addNumbers(1,2))
+    num1 = int(sys.argv[2])
+    num2 = int(sys.argv[1])
+    print(addNumbers(num1,num2))
 
 main()
