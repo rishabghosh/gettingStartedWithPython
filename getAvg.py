06ed3634b39cdcd0ba7e4880db806cdfc5282599
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-12 13:22:34 +0530

    Modified to support float inputs

--- getAvg.py
+++ getAvg.py
@@ -5,8 +5,8 @@ def addNumbers(num1, num2):
     return num1 + num2
 
 def main():
-    num1 = int(sys.argv[2])
-    num2 = int(sys.argv[1])
-    print(addNumbers(num1,num2))
+    num1 = float(sys.argv[1])
+    num2 = float(sys.argv[2])
+    print(addNumbers(num1,num2)/2)
 
 main()
